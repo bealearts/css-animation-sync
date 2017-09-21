bcae6c7ce7c4c4e9e44b6d439a90b8a6bf094008
¨David <david.beale@bealearts.co.uk>
2017-09-21 18:49:23 +0100

    Basic build

---
+++ RELEASING.md
@@ -0,0 +1,6 @@
+Releasing
+=========
+
+1. Bump the version number using ```npm version``` and push with tags
+2. Create a Release on GitHub
+3. A Travis CI build will be triggered
--- src/syncopate.js
+++ src/syncopate.js
@@ -1,14 +1,14 @@
-function syncopate(animationName) {
+export default function syncopate(animationName) {
     const elements = new Set();
     let eventTime;
 
-    window.addEventListener("animationstart", event => {
+    window.addEventListener('animationstart', event => {
         if (event.animationName === animationName) {
             elements.add(event.target);
         }
     });
 
-    window.addEventListener("animationiteration", event => {
+    window.addEventListener('animationiteration', event => {
         if (event.animationName === animationName) {
             elements.add(event.target);
 
@@ -28,31 +28,37 @@ function syncopate(animationName) {
         },
 
         start() {
-
+            elements.forEach(el => {
+                el.style.removeProperty('animation');
+            });
         },
 
         stop() {
-
+            elements.forEach(el => {
+                el.style.setProperty('animation', 'none');
+            });
         },
 
         pause() {
-
+            elements.forEach(el => {
+                el.style.setProperty('animation-play-state', 'paused');
+            });
         }
-    }
+    };
+}
 
-    function restart(elements) {
+function restart(elements) {
+    elements.forEach(el => {
+        if (window.getComputedStyle(el).animationPlayState !== 'paused') {
+            el.style.setProperty('animation', 'none');
+        }
+    });
+
+    requestAnimationFrame(() => {
         elements.forEach(el => {
-            if (window.getComputedStyle(el).animationPlayState !== "paused") {
-                el.style.setProperty("animation", "none");
+            if (window.getComputedStyle(el).animationPlayState !== 'paused') {
+                el.style.removeProperty('animation');
             }
         });
-
-        setTimeout(() => {
-            elements.forEach(el => {
-                if (window.getComputedStyle(el).animationPlayState !== "paused") {
-                    el.style.removeProperty("animation");
-                }
-            });
-        }, 10);
-    }
+    });
 }
