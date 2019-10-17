7e56878348c294ab387d423cac595cfea8b7bcf2
David Beale <david.beale@bealearts.co.uk>
2019-10-17 17:54:12 +0100

    Use babel for simple build

---
+++ example/main.js
@@ -0,0 +1,50 @@
+
+import sync from '..';
+
+let animation;
+
+function enableSync(enable) {
+  if (enable) {
+    animation = sync('spinner');
+  } else if (animation) {
+    animation.free();
+  }
+
+  enableButtons(enable);
+}
+
+function enableButtons(enable) {
+  document.querySelectorAll('Button').forEach(button => {
+    button.disabled = !enable;  // eslint-disable-line
+  });
+}
+
+function onLoad() {
+  enableButtons(false);
+
+  ['#two', '#three', '#four'].forEach((name, index) => {
+    setTimeout(() => {
+      const el = document.querySelector(name);
+      el.className += ' spinner';
+    }, 700 * (index + 1));
+  });
+}
+
+function start() {
+  animation.start();
+}
+
+function pause() {
+  animation.pause();
+}
+
+function stop() {
+  animation.stop();
+}
+
+// Export global for demo
+window.enableSync = enableSync;
+window.start = start;
+window.pause = pause;
+window.stop = stop;
+window.onload = onLoad;
